90e77fef5bedd12d71dfff51a6b14e4104e2ef44
nurali-code <nur3.dav.97@gmail.com>
2025-01-09 22:23:26 +0500

    all done

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,10 @@ document.querySelectorAll(".main-tabs__btn, .main-info__btn").forEach(button => 
 		}
 	});
 });
+
+document.querySelectorAll("#infos li").forEach(button => {
+	button.addEventListener("click", () => {
+		document.querySelectorAll("#infos li").forEach(el => el.classList.remove("active"));
+		button.classList.add('active');
+	});
+});
